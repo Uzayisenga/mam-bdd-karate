2ef54a9011a4abd52de2f9b7c4769c8241ed0e8a
Amina Uzayisenga <amina.uzayisenga.sp@dlh.de>
2025-06-24 09:44:29 +0200

    Edit KarateRunnerTest.java

--- src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
+++ src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
@@ -1,11 +1,14 @@
-package com.milesandmore.testautomation.runners;
-
+import org.junit.platform.suite.api.Suite;
 import com.intuit.karate.junit5.Karate;
 
-public class KarateRunnerTest {
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-    @Karate.Test
-    Karate testAll() {
-        return Karate.run("classpath:features").relativeTo(getClass());
-    }
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber/Cucumber.json")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.milesandmore.testautomation.stepdefinitions")
+public class KarateRunnerTest {
 }
