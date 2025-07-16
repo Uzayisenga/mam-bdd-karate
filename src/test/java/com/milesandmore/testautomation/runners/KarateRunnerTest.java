682452bde46fbc1319562a60bbb7b70cf201ce90
Uzayisenga <aminauzayisenga@gmail.com>
2025-07-16 20:27:34 +0200

    karate to run approved

--- src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
+++ src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
@@ -1,5 +1,6 @@
 package com.milesandmore.testautomation.runners;
 
+import com.intuit.karate.junit5.Karate;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -21,4 +22,10 @@ import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_N
 
 //@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@failingTest")
 public class KarateRunnerTest {
+    @Karate.Test
+    Karate runApprovedTests() {
+        return Karate.run("classpath:features")
+                .tags("@Approved")
+                .outputCucumberJson(true); // crucial for Zephyr
+    }
 }
