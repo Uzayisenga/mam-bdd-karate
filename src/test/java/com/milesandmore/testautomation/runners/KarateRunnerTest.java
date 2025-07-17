b780e66b8673ee317cebc94a45256e00e9ce8f5f
Uzayisenga <aminauzayisenga@gmail.com>
2025-07-17 13:19:42 +0200

    karate

--- src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
+++ src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
@@ -1,13 +1,36 @@
 package com.milesandmore.testautomation.runners;
 
 import com.intuit.karate.junit5.Karate;
+import java.io.File;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
+import org.junit.jupiter.api.TestFactory;
+
+import static com.intuit.karate.resource.ResourceUtils.findFeatureFiles;
 
 public class KarateRunnerTest {
 
     @Karate.Test
-    Karate runApprovedTests() {
-        return Karate.run("classpath:features")
-                .tags("@Approved")
-                .outputCucumberJson(true);
+    Karate testAllApproved() {
+        return Karate.run("classpath:features").tags("@Approved").relativeTo(getClass());
+    }
+
+    Stream<Karate> runAllApprovedTests() {
+        File featureDir = new File("src/test/resources/features");
+
+        List<File> approvedFeatureFiles = findFeatureFiles(featureDir);
+
+        return approvedFeatureFiles.stream()
+                .map(file -> Karate.run(file.getPath())
+                        .tags("@Approved")
+                        .relativeTo(getClass()));
+    }
+
+    private List<File> findFeatureFiles(File dir) {
+        return Stream.ofNullable(dir.listFiles())
+                .flatMap(Stream::of)
+                .filter(file -> file.isFile() && file.getName().endsWith(".feature"))
+                .collect(Collectors.toList());
     }
 }
