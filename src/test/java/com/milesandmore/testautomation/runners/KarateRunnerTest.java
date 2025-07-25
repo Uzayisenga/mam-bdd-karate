f82d8950971c4ba6c6c63459b323cae01ed84676
Uzayisenga <aminauzayisenga@gmail.com>
2025-07-25 12:27:22 +0200

    Changed the logs

--- src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
+++ src/test/java/com/milesandmore/testautomation/runners/KarateRunnerTest.java
@@ -6,8 +6,8 @@ public class KarateRunnerTest {
 
     @Karate.Test
     Karate runApprovedTests() {
-        return Karate.run("classpath:features")  // This tells Karate to look in src/test/resources/features
-                .tags("@Approved")               // If you want to filter by tag
-                .outputCucumberJson(true);       // For Zephyr upload
+        return Karate.run("classpath:features")
+                .tags("@Approved")
+                .outputCucumberJson(true);
     }
 }
